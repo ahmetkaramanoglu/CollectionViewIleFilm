fb8005925865fffcdbc105c407e71b49c954edc1
Ahmet <ahmetkaramanoglu678@gmail.com>
2023-12-15 02:43:16 +0300

    Initial Commit

---
+++ CollectionIleFilmUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionIleFilmUygulamasi
+//
+//  Created by Ahmet Karamanoğlu on 15.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
